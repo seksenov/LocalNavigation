d7d2a56afb52e4791350709345cb7658cd1c9f4a
Kiril Seksenov <seksenovk@gmail.com>
2016-03-15 19:14:33 -0700

    Update 2016-03-16T02:14:30.717Z

--- gulpfile.babel.js/tasks/clean.js
+++
@@ -1,7 +0,0 @@
-import gulp from 'gulp';
-import config from '../config';
-import del from 'del';
-
-gulp.task('clean', done => {
-  del([config.clean.src], done);
-});
--- gulpfile.babel.js/tasks/deploy.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import ghPages from 'gulp-gh-pages';
-import config from '../config';
-
-gulp.task('deploy', () => {
-  return gulp.src(config.deploy.src)
-  .pipe(ghPages())
-});
