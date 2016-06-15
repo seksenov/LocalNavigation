191a26c94ea28868d8625c7027cb425020e7afd2
Kiril Seksenov <seksenovk@gmail.com>
2016-06-15 12:22:45 -0700

    Adding button nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("btn").addEventListener("click", navigate);
+});
+
+function navigate() {
+  window.location.href("https://twitter.com/k_seks");
+}
